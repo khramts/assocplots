77f4cedb9d2d923bf12ab72116f35fc1f794bb5a
Katya Khramtsova <eakhram@uchicago.edu>
2015-12-30 22:25:01 -0600

    bokeh added

---
+++ assocplots/mann.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def reduce_data(data, top_snps=1000):
+    '''
+    Function reduces data for interactive plot
+    :param data: list of datasets
+    :param top_snps: number of snps to include
+    :return: combined table
+    '''
---
+++ assocplots/misc.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def mock_data_generation(N=2, M=10000):
+    '''
+    Generation of mock catalogues
+    :param N: number of catalogs
+    :param M: number of snps per catalog
+    :return: list of catalogs
+    '''
+    res = []
+    for k in range(N):
+        pos = np.arange(M)
+        pval = np.random.rand(M) / (np.random.rand(M)<0.999)
+        x = [("rs%04d"%(i), pos[i], pval[i]) for i in pos[np.random.permutation(M)]]
+        x = np.array(x, dtype=[('snp', "|S20"), ('pos', np.int), ('pval', float)])
+        res.append(x)
+    return res
+
+def data_reduce_2(data1,data2,N=100):
+    '''
+    Reduces data for interactive plot
+    :param data: list of catalogs
+    :return: table
+    '''
+    ind1 = data1['pval'].argsort()[N:][::-1]
+    ind2 = data2['pval'].argsort()[N:][::-1]
+    ind = np.array(list(set([ind1,ind2])))
