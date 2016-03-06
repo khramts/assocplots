bffc02d0bf0e26eb44b33152d6dbc1afaeea138c
Katya Khramtsova <eakhram@uchicago.edu>
2016-03-05 19:32:02 -0600

    setup script added

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "assocplots",
+    version = "0.0.1",
+    author = "Ekaterina",
+    author_email = "eakhram@gmail.com",
+    description = ("AssocPlots"),
+    license = "MIT",
+    keywords = "manhattan qqplot Quantile-Quantile plots",
+    url = "https://github.com/khramts/assocplots",
+    packages=['assocplots'],
+    long_description=read('README.md'),
+    install_requires=[
+          'matplotlib', 'bokeh', 'numpy', 'scipy'
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+    ],
+)
