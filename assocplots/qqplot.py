0f84441a4c3b16609ecee69c2ca67a5c575ef964
Alexander Kaurov <akaurov@gmail.com>
2016-03-06 13:58:39 -0600

    Normal distribution error added to QQplot (binomial distribution is a better approximation, but less visually attractive)

--- assocplots/qqplot.py
+++ assocplots/qqplot.py
@@ -2,9 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats.mstats import mquantiles
 from scipy.stats import binom
+from scipy.stats import norm
 from scipy.stats import linregress
 
-def qqplot(data, labels, n_quantiles=100, alpha=0.95, error_type='theoretical', log10conv=True, color='k', type = 'uniform'):
+def qqplot(data, labels, n_quantiles=100, alpha=0.95, error_type='theoretical', distribution = 'binomial', log10conv=True, color=['k', 'r', 'b'], fill_dens=[0.1, 0.1, 0.1], type = 'uniform'):
     '''
     Function for plotting Quantile Quantile (QQ) plots with confidence interval (CI)
     :param data: NumPy 1D array with data
@@ -26,21 +27,28 @@ def qqplot(data, labels, n_quantiles=100, alpha=0.95, error_type='theoretical', 
             q_th = q_pos.copy()
             # evaluate errors
             q_err = np.zeros([len(q_pos),2])
-            if alpha>0:
-                for i in range(0, n_quantiles):
-                    q_err[i,:] = binom.interval(alpha=alpha, n=len(data[j]), p=q_pos[i])
+            if alpha > 0:
+                for i in range(0, len(q_pos)):
+                    if distribution == 'binomial':
+                        q_err[i, :] = binom.interval(alpha=alpha, n=len(data[j]), p=q_pos[i])
+                    elif distribution == 'normal':
+                        q_err[i, :] = norm.interval(alpha, len(data[j])*q_pos[i], np.sqrt(len(data[j])*q_pos[i]*(1.-q_pos[i])))
+                        q_err[i, q_err[i, :] < 0] = 1e-12
+                    else:
+                        print('Distribution is not defined!')
                 q_err /= 1.0*len(data[j])
                 for i in range(0, 100):
                     q_err[i,:] += 1e-12
+            # print(q_err[100:, :])
             slope, intercept, r_value, p_value, std_err = linregress(q_th, q_data)
             print(labels[j], ' -- Slope: ', slope, " R-squared:", r_value**2)
             plt.plot(-np.log10(q_th), -np.log10(q_data), '.', color=color[j], label=labels[j])
             if alpha>0:
                 if error_type=='experimental':
-                    plt.fill_between(-np.log10(q_th), -np.log10(q_data/q_th*q_err[:,0]), -np.log10(q_data/q_th*q_err[:,1]), color=color[j], alpha=0.1, label='%1.3f CI'%alpha)
+                    plt.fill_between(-np.log10(q_th), -np.log10(q_data/q_th*q_err[:,0]), -np.log10(q_data/q_th*q_err[:,1]), color=color[j], alpha=fill_dens[j], label='%1.3f CI'%alpha)
         if alpha>0:
             if error_type=='theoretical':
-                plt.fill_between(-np.log10(q_th), -np.log10(q_err[:,0]), -np.log10(q_err[:,1]), color='k', alpha=0.1, label='%1.3f CI'%alpha)
+                plt.fill_between(-np.log10(q_th), -np.log10(q_err[:,0]), -np.log10(q_err[:,1]), color=color[j], alpha=fill_dens[j], label='%1.3f CI'%alpha)
     plt.legend(loc=4)
     plt.xlabel('Theoretical -log10')
     plt.ylabel('Experimental -log10')
